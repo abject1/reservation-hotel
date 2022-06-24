758b655bf13daab416a99e549d5c3c1aefc5e573
Abject <lucaswurtz0@gmail.com>
2022-06-24 20:11:09 +0200

    mise en place header et qui suis je

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log("yes !");
+// DECLARATION DE VARIABLES
+const menuBurger = document.getElementById("menuBurger");
+const nav = document.getElementById("nav");
+const btnPres = document.getElementById("btnPresentation");
+const nextTextPres = document.getElementById("nextPresentation");
+
+// OUVERTURE DU MENU BURGER
+
+menuBurger.addEventListener("click", () => {
+  nav.classList.toggle("navSeen");
+});
+
+// AFFICHER LA SUITE DE LA PRESENTATION
+
+btnPres.addEventListener("click", () => {
+  nextTextPres.classList.toggle("nextSeenPresentation");
+});
